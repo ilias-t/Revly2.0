bb1abd580ba694038c7816b5b19e9ebc0f573b40
iliastsangaris <iliastsangaris@gmail.com>
2014-04-03 20:15:34 -0400

    users exist, have secure passwords, and are validated

--- Gemfile
+++ Gemfile
@@ -13,7 +13,7 @@ gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
 
 # Use CoffeeScript for .js.coffee assets and views
-# gem 'coffee-rails', '~> 4.0.0'
+gem 'coffee-rails', '~> 4.0.0'
 
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
@@ -26,6 +26,9 @@ gem 'turbolinks'
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
+
+# Login library
+gem 'devise'
 
 # Use ActiveModel has_secure_password
 gem 'bcrypt-ruby', '~> 3.1.2'
--- spec/features/user_can_sign_up.rb
+++ spec/features/user_can_sign_up.rb
@@ -8,16 +8,16 @@ describe "user can sign up", js: true do
 
   it "can sign up" do 
       visit root_path
-      find("h2#sign-up-link").click
-      fill_in "signup-name", with: "Steve"
-      fill_in "signup-email", with: "Steve@steve.com"
-      fill_in "signup-password", with: "swordfish"
-      fill_in "signup-password-confirmation", with: "swordfish"
-      
-      # save_and_open_page
-      click_button "sign-up"
-      steve = User.find_by(name: "Steve")
-      expect(steve).to_not be_nil
-      # expect(page).to have_content "Steve"
+      within("#signup-form") do
+        fill_in "name", with: "Ilias"
+        fill_in "email", with: "i@t.com"
+        fill_in "username", with: "itsangaris"
+        fill_in "password", with: "password"
+        fill_in "password_confirmation", with: "password"
+      end
+      click_link "Sign up"
+      ilias = User.find_by(name: "Ilias")
+      expect(ilias).to_not be_nil
+      expect(page).to have_content "Ilias"
   end
 end
